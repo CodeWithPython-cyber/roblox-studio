99774d72a9b34e9278907ba51453bb2077046770
Enzo Gabriel <enzogabsn@gmail.com>
2025-03-01 19:52:12 -0300

    DoubleJumps.lua

--- StarterPlayer/StarterCharacterScripts/DoubleJumps.lua
+++ StarterPlayer/StarterCharacterScripts/DoubleJumps.lua
@@ -1,1 +1,34 @@
+local UIS = game:GetService("UserInputService")
+local Humanoid = script.Parent:WaitForChild("Humanoid")
 
+local db = true
+local CanDoubleJump, DoubleJumped = false, false
+
+local function JumpRequest()
+	if db then
+		db = false
+		
+		if CanDoubleJump and not DoubleJumped then
+			DoubleJumped = true
+			Humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+		end
+		
+		task.wait(.2)
+		db = true
+	end
+end
+
+UIS.JumpRequest:Connect(JumpRequest)
+
+local function StateChanged(_, new)
+	if new == Enum.HumanoidStateType.Landed then
+		CanDoubleJump = false
+		DoubleJumped = false
+		
+	elseif new == Enum.HumanoidStateType.Freefall and not DoubleJumped then
+		CanDoubleJump = true
+		DoubleJumped = false
+	end
+end
+
+Humanoid.StateChanged:Connect(StateChanged)
